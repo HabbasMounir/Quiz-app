0effc51a5276657c558aab462ef99685016307d4
Mounir habbas <Mounirawt@gmail.com>
2022-07-30 17:29:53 +0100

    Quiz-app V:02.06.05

--- state/main.js
+++ state/main.js
@@ -6,7 +6,7 @@ let   QuestionArea=document.getElementById("Quiz__area")
 
 let QuestionCount=0;
 let rightAnswers=0;
-
+let bulletArry=[];
 
 
 function getQuestion() {
@@ -15,106 +15,114 @@ function getQuestion() {
 
 myReauest.onreadystatechange=function () {
     if(myReauest.status===200 && myReauest.readyState===4){
-        // console.log(myReauest)
                 let Question= JSON.parse(this.responseText)
                 let QuestionCounter=Question.length
-        console.log(QuestionCounter)
-
     //bullets creation
         bulletsCreator(QuestionCounter)
 
         // add Data
-        addQuestionData(Question[QuestionCount]);
+        addQuestionData(Question[QuestionCount],QuestionCounter);
 
         //click on submit
       submitButton.onclick = () => {
         // GET RIGHT ANSWER:
         let rightAnswer = Question[QuestionCount].rightanswer;
 
-        // QuestionCount++
-        
-        // check
-        checkRightnswer(rightAnswer,QuestionCounter);
 
-};
+        // check
+                checkRightnswer(rightAnswer,QuestionCounter);
+
+
+                  QuestionCount++
+        
+
+
+        // remove
+        // bullets.innerHTML=""
+        myanswers.innerHTML=""
+        QuestionArea.innerHTML=""
+
+
+
+        addQuestionData(Question[QuestionCount],QuestionCounter); 
+        ShowResulte(QuestionCounter);
+};  
     }
 }
 
-
-
 myReauest.open("GET","./state/QU.json",true);
 myReauest.send();
-
-
-
-
 }
 
 getQuestion()
+
+
 
 
 function bulletsCreator(num) {
 
     // creat spans
     for (let i = 0; i < num; i++) {
+
             // creat span
         let    bullet=document.createElement("span")
-
         i<=QuestionCount  ?bullet.className="bullet--active":"";
         bullets.appendChild(bullet)
 
     }
 }
-function addQuestionData(obj){
 
-    console.log(obj);
-    let QuestionTitle=document.createElement("h2");
-    QuestionTitle.innerHTML=`${obj["title"]} :`
-    QuestionArea.appendChild(QuestionTitle);
+function addQuestionData(obj,QuCounter){
+if ( QuestionCount<QuCounter) {
+      // console.log(obj);
+      let QuestionTitle=document.createElement("h2");
+      QuestionTitle.innerHTML=`${obj["title"]} :`
+      QuestionArea.appendChild(QuestionTitle);
+  
+  
+      let answrSize=Object.keys(obj).length-2  //2(title + Right answer)
+      // answer
+  
+      for(i=1;i<=answrSize;i++){
+      let QuestionAnswer=document.createElement("div");
+          QuestionAnswer.className="answer" 
+  
+                          // Create Radio Input
+      let radioInput = document.createElement("input");
+  
+        // Add Type + Name + Id + Data-Attribute
+      radioInput.name = "answer";
+      radioInput.type = "radio";
+      radioInput.id = `answer_0${i}`;
+      radioInput.dataset.answer = obj[`answer_0${i}`];
+  
+        // Make First Option Selected
+      if (i === 1) {
+          radioInput.checked = true;
+      }
+  
+        // Create Label
+      let theLabel = document.createElement("label");
+      theLabel.className=`labele--active`
+        // Add For Attribute
+      theLabel.htmlFor = `answer_0${i}`;
+  
+        // Create Label Text
+      let theLabelText = document.createTextNode(obj[`answer_0${i}`]);
+  
+        // Add The Text To Label
+      theLabel.appendChild(theLabelText);
+  
+        // Add Input + Label To Main Div
+      QuestionAnswer.appendChild(radioInput);
+      QuestionAnswer.appendChild(theLabel);
+  
+        // Append All Divs To Answers Area
+          myanswers.appendChild(QuestionAnswer);
+  
+      }
+}
 
-
-    let answrSize=Object.keys(obj).length-2  //2(title + Right answer)
-    // answer
-
-    for(i=1;i<=answrSize;i++){
-        console.log(i)
-    let QuestionAnswer=document.createElement("div");
-        QuestionAnswer.className="answer" 
-
-                        // Create Radio Input
-    let radioInput = document.createElement("input");
-
-      // Add Type + Name + Id + Data-Attribute
-    radioInput.name = "answer";
-    radioInput.type = "radio";
-    radioInput.id = `answer_0${i}`;
-    radioInput.dataset.answer = obj[`answer_0${i}`];
-
-      // Make First Option Selected
-    if (i === 1) {
-        radioInput.checked = true;
-    }
-
-      // Create Label
-    let theLabel = document.createElement("label");
-    theLabel.className=`labele--active`
-      // Add For Attribute
-    theLabel.htmlFor = `answer_0${i}`;
-
-      // Create Label Text
-    let theLabelText = document.createTextNode(obj[`answer_0${i}`]);
-
-      // Add The Text To Label
-    theLabel.appendChild(theLabelText);
-
-      // Add Input + Label To Main Div
-    QuestionAnswer.appendChild(radioInput);
-    QuestionAnswer.appendChild(theLabel);
-
-      // Append All Divs To Answers Area
-        myanswers.appendChild(QuestionAnswer);
-
-    }
 }
 function checkRightnswer(rigAns,QueCou){
     let answers=document.getElementsByName("answer")
@@ -127,17 +135,33 @@ function checkRightnswer(rigAns,QueCou){
         labels[i].classList.remove("answer--right")   
 
         if(answers[i].checked === true){
-
+            let  bulleter=document.querySelectorAll(".bullets span")
             choosenAnswer=answers[i].dataset.answer
 
         if (choosenAnswer===rigAns) {
+          rightAnswers++;
             labels[i].classList.add("answer--right")
+            bulletArry[bulletArry.length]=true;
         } else{
             labels[i].classList.add("answer--wrong")
+            bulletArry[bulletArry.length]=false;
         }
         answers[i].checked = false;
-        }
+        bulletArry[i]===true  ?bulleter[QuestionCount].className="bullet--right":bulleter[QuestionCount].className="bullet--wrong";
+        QuestionCount+1 <bulleter.length ?bulleter[QuestionCount+1].className="bullet--active":"";
+
+        
+      }
     }
-    console.log(rigAns)
-    console.log(choosenAnswer)
+
 }
+
+
+
+function ShowResulte(Counter){
+if(QuestionCount===Counter){
+console.log("ydek fih")
+submitButton.style.display="none"
+}
+}
+
